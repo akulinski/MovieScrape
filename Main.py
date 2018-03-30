fee6eb8f126d88aa59b33dcc1996417b71caaf66
albert <albert.kulinski@hotmail.com>
2018-03-31 00:25:24 +0200

    Scraping imsb working

--- Main.py
+++ Main.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup as Soup
 import CSVWrite
 
 import scrape_filmweb
+import scrape_imdb
 
 def main():
 
     filmweb=scrape_filmweb.scraper()
     filmweb.scrape()
 
-
+    imdb=scrape_imdb.scraper()
+    imdb.scrape()
 
 if __name__ == '__main__':
     main()
--- scrape_imdb.py
+++ scrape_imdb.py
@@ -4,26 +4,25 @@ import CSVWrite
 
 class scraper:
 
-    filmweb_top500 = "http://www.filmweb.pl/ranking/film"
-
+    link='http://www.imdb.com/list/ls004427773/'
     def __init__(self):
-        self.uClient = uReq(scraper.filmweb_top500)
+        self.uClient = uReq(scraper.link)
         self.page_html = self.uClient.read()
         self.uClient.close()
         self.page_soup = Soup(self.page_html, "html.parser")
 
     def scrape(self):
+        self.containers = self.page_soup.find_all("div", {"class": "lister-item-content"})
 
-        self.containers = self.page_soup.find_all("div", {"class": "item place"})
-
-        writer = CSVWrite.Writer("dataFilmWeb")
+        writer = CSVWrite.Writer("dataImdb")
         count=0
         for container in self.containers:
-            titles = container.findAll("div", {"class": "film__original"})
-            rating = container.findAll("span", {"class": "rate__value"})
+            titles = container.find_all("a")
+            rating = container.find_all("strong")
             try:
                 print(titles[0].text + " " + rating[0].text)
                 print(count)
                 writer.wirteToFile(titles[0].text, rating[0].text)
+                count+=1
             except IndexError:
                 pass
