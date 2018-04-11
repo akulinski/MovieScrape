818b82804ef8c524d7b9794271181bb70a58ef33
Albert <albert.kulinski@hotmail.com>
2018-04-11 20:06:47 +0200

    create table of common and facebook

--- Main.py
+++ Main.py
@@ -5,10 +5,7 @@ import DatabaseController
 import scrapeRottenTomatoes
 import FacebookController
 import TOKEN
-<<<<<<< HEAD
-
-=======
->>>>>>> 8502a2519c54cba741527620e507c32c0dbf386b
+##
 def main():
 
 #starting point invoking methods from classes, scraping data info csv and uploding csv to db
